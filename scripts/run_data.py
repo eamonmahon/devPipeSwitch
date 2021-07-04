d7099b703c6faaf7fe8971d6479dc2a7ff825c6c
Zhihao Bai <zbai1@jhu.edu>
2021-07-03 23:43:29 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -1,9 +1,11 @@
+import time
 import subprocess
 
 batch_size = 8
 
 def main():
-    p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/model_list.txt'])
+    p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'])
+    time.sleep(30)
     p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)])
     p_client.wait()
     p_server.kill()
