66a60eafe4241952febd03e86c3fbe42adf21e67
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 01:47:53 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -6,9 +6,9 @@ batch_size = 8
 
 def main():
     with open(os.devnull, 'w') as fnull:
-        p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'])
+        p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)])
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
         p_client.wait()
         p_server.kill()
 
