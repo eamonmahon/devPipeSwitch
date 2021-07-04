1ace847fa056680eb295afb22ca0c60939e356fc
Zhihao Bai <zbai1@jhu.edu>
2021-07-04 00:54:50 -0400

    Supress output of the server

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -5,11 +5,12 @@ import subprocess
 batch_size = 8
 
 def main():
-    p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'], stdout=os.devnull, stderr=os.devnull)
-    time.sleep(30)
-    p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)])
-    p_client.wait()
-    p_server.kill()
+    with open(os.devnull, 'w') as fnull:
+        p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'], stdout=fnull, stderr=fnull)
+        time.sleep(30)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)])
+        p_client.wait()
+        p_server.kill()
 
 if __name__ == '__main__':
     main()
