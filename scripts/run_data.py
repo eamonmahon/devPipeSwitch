dd06cfa8fcbc7a7303b18f8ce3ad26638af00d28
Zhihao Bai <zbai1@jhu.edu>
2021-07-05 18:34:16 -0400

    Update

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -9,7 +9,7 @@ def main():
         p_server = subprocess.Popen(['python','PipeSwitch/ready_model/ready_model.py','resnet152'], stdout=fnull, stderr=fnull)
         time.sleep(30)
 
-        scheduling_cycle = 5
+        scheduling_cycle = 10
         p_client = subprocess.Popen(['python','PipeSwitch/client/throughput_ready.py', 'resnet152', str(batch_size), scheduling_cycle])
 
         p_client.wait()
