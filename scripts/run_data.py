c6bc13a9d2cdb79b660874814e6191f4c27eb37d
Zhihao Bai <zbai1@jhu.edu>
2021-07-05 20:38:15 -0400

    Update

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -6,11 +6,11 @@ batch_size = 8
 
 def main():
     with open(os.devnull, 'w') as fnull:
-        p_server = subprocess.Popen(['python','PipeSwitch/ready_model/ready_model.py','resnet152'], stdout=fnull, stderr=fnull)
+        p_server = subprocess.Popen(['python', 'PipeSwitch/ready_model/ready_model.py','resnet152'], stdout=fnull, stderr=fnull)
         time.sleep(30)
 
         scheduling_cycle = 10
-        p_client = subprocess.Popen(['python','PipeSwitch/client/throughput_ready.py', 'resnet152', str(batch_size), scheduling_cycle])
+        p_client = subprocess.Popen(['python', 'PipeSwitch/client/throughput_ready.py', 'resnet152', str(batch_size), scheduling_cycle])
 
         p_client.wait()
         p_server.kill()
