30b187f55cf220e2fa8a4cdf9360da8480a9fcfb
Zhihao Bai <zbai1@jhu.edu>
2021-07-06 00:50:12 -0400

    Test with another version for kill_restart

--- kill_restart/kill_restart.py
+++ kill_restart/kill_restart.py
@@ -6,9 +6,15 @@ import importlib
 
 import torch.multiprocessing as mp
 
+# from core.checkpoint_server import CheckpointProc
 from util.util import TcpServer, TcpAgent, timestamp
+from sys import argv
 
 def func_get_request(qout):
+    # Start checkpoint serser
+    # ckpt = CheckpointProc()
+    # ckpt.start()
+
     # Listen connections
     server = TcpServer('localhost', 12345)
 
@@ -36,22 +42,36 @@ def func_get_request(qout):
 
 def func_schedule(qin):
     active_worker = None
+    active_task = None
+    active_pipe = None
     while True:
         agent, model_name, data_b = qin.get()
-        if active_worker is not None:
-            active_worker.terminate()
-            active_worker.join()
-        active_worker = mp.Process(target=worker_compute, args=(agent, model_name, data_b))
-        active_worker.start()
+        timestamp('schedule get model name', model_name)
+        if model_name == active_task:
+            active_pipe.send((agent, model_name, data_b))
+        else:
+            if active_worker is not None:
+                timestamp('Killing active worker', '')
+                active_worker.kill()
+                active_worker.join()
+            parent_p, child_p = mp.Pipe()
+            active_pipe = parent_p
+            active_task = model_name
+            active_worker = mp.Process(target=worker_compute, args=(agent, model_name, data_b, child_p))
+            active_worker.start()
+            timestamp('schedule', 'new worker started for {}'.format(model_name))
 
-def worker_compute(agent, model_name, data_b):
+def worker_compute(agent, model_name, data_b, pipe):
     # Load model
     model_module = importlib.import_module('task.' + model_name)
     model, func, _ = model_module.import_task()
     data_loader = model_module.import_data_loader()
 
+    timestamp('worker comp proc', 'model loaded to cpu')
+
     # Model to GPU
     model = model.to('cuda')
+    timestamp('****workr', 'loaded to cuda')
 
     # Compute
     if 'training' in model_name:
@@ -68,9 +88,15 @@ def worker_compute(agent, model_name, data_b):
 
         agent.send(b'FNSH')
         del agent
-        timestamp('server', 'reply')
+        timestamp('server', 'complete inf ')
 
-    
+    while True:
+        # for continous inference task
+        agent, model_name, data_b = pipe.recv()
+        output = func(model, data_b)
+        agent.send(b'FNSH')
+        del agent
+        timestamp('server', 'complete inf and reply')
 
 def main():
     # Create threads and worker process
