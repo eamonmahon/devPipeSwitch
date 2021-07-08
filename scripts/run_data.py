f9280cf7fcec36ce8432f09011fbb3633228af3a
Zhihao Bai <zbai1@jhu.edu>
2021-07-08 04:40:13 -0400

    Update

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -7,9 +7,9 @@ batch_size = 8
 def main():
     os.system("nvidia-cuda-mps-control -d")
     with open(os.devnull, 'w') as fnull:
-        p_server = subprocess.Popen(['python','PipeSwitch/mps/server_nonstop.py','resnet152'], stdout=fnull, stderr=fnull)
+        p_server = subprocess.Popen(['python','PipeSwitch/mps/server_nonstop.py','inception_v3'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)], stderr=fnull)
         p_client.wait()
         p_server.kill()
 
