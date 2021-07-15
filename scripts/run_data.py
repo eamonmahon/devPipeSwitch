8769f235aacd9a9533ea6070301d1407056954c3
Zhihao Bai <zbai1@jhu.edu>
2021-07-14 20:09:21 -0400

    Update

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -9,7 +9,11 @@ def main():
     with open(os.devnull, 'w') as fnull:
         p_server = subprocess.Popen(['python','PipeSwitch/mps/server_nonstop.py','resnet152'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
+
+        scheduling_cycle = 1
+        interval_count = 10
+        p_client = subprocess.Popen(['python', 'PipeSwitch/client/throughput_ready.py', str(scheduling_cycle), str(interval_count)], stderr=fnull)
+
         p_client.wait()
         p_server.kill()
 
