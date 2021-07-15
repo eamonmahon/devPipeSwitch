1af4dc15aeb19eafad57bf83266acde9749fe36d
Zhihao Bai <zbai1@jhu.edu>
2021-07-15 18:53:24 -0400

    Update

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -6,9 +6,9 @@ batch_size = 8
 
 def main():
     with open(os.devnull, 'w') as fnull:
-        p_server = subprocess.Popen(['python','PipeSwitch/ready_model/ready_model.py','resnet152'], stdout=fnull, stderr=fnull)
+        p_server = subprocess.Popen(['python','PipeSwitch/memory_malloc/memory_malloc.py','PipeSwitch/memory_malloc/model_list.txt'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_inference.py', 'resnet152', str(batch_size)], stderr=fnull)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
         p_client.wait()
         p_server.kill()
 
