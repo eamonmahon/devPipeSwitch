f1b76ff9e903a174048918008cbed0ea3ebeeb0e
Zhihao Bai <zbai1@jhu.edu>
2021-07-21 22:07:46 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -8,7 +8,8 @@ def main():
     with open(os.devnull, 'w') as fnull:
         p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','bert_base'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'bert_base', str(batch_size)], stderr=fnull)
+        # p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'bert_base', str(batch_size)], stderr=fnull)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'bert_base', str(batch_size)])
         p_client.wait()
         p_server.kill()
 
