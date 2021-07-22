f83c52fe7ccbf4a521a2d450c8005a0d1d5cc514
Zhihao Bai <zbai1@jhu.edu>
2021-07-21 23:03:52 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -10,7 +10,14 @@ def main():
         time.sleep(30)
         # p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)], stderr=fnull)
         p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)])
-        p_client.wait()
+        while True:
+            try:
+                p_client.wait(1)
+                break
+            except:
+                print (time.time())
+                sys.stdout.flush()
+                continue
         p_server.kill()
 
 if __name__ == '__main__':
