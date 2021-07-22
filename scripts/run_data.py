336c9c710b048428b5dff5883a42f0ea4dc02dcb
Zhihao Bai <zbai1@jhu.edu>
2021-07-21 20:29:09 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -6,11 +6,18 @@ batch_size = 8
 
 def main():
     with open(os.devnull, 'w') as fnull:
-        p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','resnet152'], stdout=fnull, stderr=fnull)
+        print ('Run 1')
+        # p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','resnet152'], stdout=fnull, stderr=fnull)
+        p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','resnet152'])
+        print ('Run 2')
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
+        # p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)], stderr=fnull)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'resnet152', str(batch_size)])
+        print ('Run 3')
         p_client.wait()
+        print ('Run 4')
         p_server.kill()
+        print ('Run 5')
 
 if __name__ == '__main__':
     main()
