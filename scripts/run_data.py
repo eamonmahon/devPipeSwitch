cc5a00952d65f1bf2ed8a8772ef0c63ac1c803bd
Zhihao Bai <zbai1@jhu.edu>
2021-07-21 22:07:25 -0400

    Debug

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -8,7 +8,8 @@ def main():
     with open(os.devnull, 'w') as fnull:
         p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','inception_v3'], stdout=fnull, stderr=fnull)
         time.sleep(30)
-        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)], stderr=fnull)
+        # p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)], stderr=fnull)
+        p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)])
         p_client.wait()
         p_server.kill()
 
