062828cba49cf985846ded2bcd9550b9fb9f0be5
Zhihao Bai <zbai1@jhu.edu>
2021-10-19 22:50:23 -0400

    Update batch size

--- task/resnet152_training.py
+++ task/resnet152_training.py
@@ -20,7 +20,7 @@ def import_func():
     def train(model, data_loader):
         # Prepare data
         # batch_size = 32
-        batch_size = 72
+        batch_size = 80
         images, target = data_loader(batch_size)
 
         # Prepare training
