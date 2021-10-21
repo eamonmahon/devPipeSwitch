a88fddc58fd17e40ac0f90e33fc3fb798b4a715f
Zhihao Bai <zbai1@jhu.edu>
2021-10-21 00:08:58 -0400

    remove unused codes

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import subprocess
 
@@ -9,7 +10,14 @@ def main():
         p_server = subprocess.Popen(['python','PipeSwitch/pipeswitch/main.py','PipeSwitch/pipeswitch/model_list.txt'], stdout=fnull, stderr=fnull)
         time.sleep(30)
         p_client = subprocess.Popen(['python','PipeSwitch/client/client_switching.py', 'inception_v3', str(batch_size)], stderr=fnull)
-        p_client.wait()
+        while True:
+            try:
+                p_client.wait(1)
+                break
+            except:
+                print (time.time())
+                sys.stdout.flush()
+            continue
         p_server.kill()
 
 if __name__ == '__main__':
