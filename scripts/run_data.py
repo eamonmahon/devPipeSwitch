2bd03d8249eb879fa6ea85bb9f3385b3fd021a1f
Zhihao Bai <zbai1@jhu.edu>
2021-10-20 23:22:16 -0400

    Merge

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -9,7 +9,7 @@ def main():
         p_server = subprocess.Popen(['python','PipeSwitch/kill_restart/kill_restart.py','resnet152'], stdout=fnull, stderr=fnull)
         time.sleep(30)
 
-        scheduling_cycle = 1
+        scheduling_cycle = 10
         interval_count = 10
         p_client = subprocess.Popen(['python', 'PipeSwitch/client/throughput.py', str(scheduling_cycle), str(interval_count)], stderr=fnull)
 
