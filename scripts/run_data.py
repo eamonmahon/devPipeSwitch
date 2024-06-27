85d150499816d98705ff8eb1e44fe5a8f362efe4
eamonmahon <s2602050@ed.ac.uk>
2024-06-27 10:16:47 +0000

    adding print statement for debugging

--- scripts/run_data.py
+++ scripts/run_data.py
@@ -5,6 +5,7 @@ import subprocess
 batch_size = 8
 
 def main():
+    print("running run_data.py from figure5_ready_model_resnet152 branch")
     with open(os.devnull, 'w') as fnull:
         p_server = subprocess.Popen(['python','PipeSwitch/ready_model/ready_model.py','resnet152'], stdout=fnull, stderr=fnull)
         time.sleep(30)
