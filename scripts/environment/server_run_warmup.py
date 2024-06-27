5d7accaddeea5312b09d55dbf6d634887f6b6031
eamonmahon <s2602050@ed.ac.uk>
2024-06-27 11:58:30 +0000

    adding print statement for debugging

--- scripts/environment/server_run_warmup.py
+++ scripts/environment/server_run_warmup.py
@@ -4,6 +4,7 @@ import sys
 from scripts.common.util import RunDocker
 
 def main():
+    print("Running: RunDocker('pipeswitch:ready_model', dev) as rd:")
     with RunDocker('pipeswitch:ready_model', 'dev') as rd:
         # Start the server: ready_model
         rd.run('python PipeSwitch/scripts/environment/container_run_warmup.py')
