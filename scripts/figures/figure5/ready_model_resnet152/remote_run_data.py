2aba8ed22f67be8e714d32cac4f3b1ef6fcb420b
eamonmahon <s2602050@ed.ac.uk>
2024-06-27 10:10:43 +0000

    add comment to check why figure 5 isn't running at all

--- scripts/figures/figure5/ready_model_resnet152/remote_run_data.py
+++ scripts/figures/figure5/ready_model_resnet152/remote_run_data.py
@@ -6,6 +6,7 @@ from scripts.common.util import RunDocker
 def main():
     with RunDocker('pipeswitch:ready_model', 'figure5_ready_model_resnet152') as rd:
         # Start the server: ready_model
+        print("Attempting to run run_data.py from figure5_ready_model_resnet152 branch.")
         rd.run('python PipeSwitch/scripts/run_data.py')
         
         # Get and return the data point
