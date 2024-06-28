f4368c6a25da05f1f8898dcc6c609b4d2e353ce5
eamonmahon <s2602050@ed.ac.uk>
2024-06-28 14:58:21 +0000

    more debugging

--- scripts/environment/server_run_warmup.sh
+++ scripts/environment/server_run_warmup.sh
@@ -1,7 +1,9 @@
 # Get current work dir
 WORK_DIR=$(pwd)/PipeSwitch
-
+echo "error check1"
 # Import global variables
 source $WORK_DIR/scripts/config/env.sh
+echo "error check2"
 
 PYTHONPATH=$PYTHONPATH:$WORK_DIR python $WORK_DIR/scripts/environment/server_run_warmup.py
+echo "error check3"
