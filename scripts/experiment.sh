75bb22be407e6bdfb5f37b66f310e4467f66dec0
eamonmahon <s2602050@ed.ac.uk>
2024-07-01 15:26:02 +0000

    runnning experiment with upgraded versions

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -1,19 +1,19 @@
 # ##############################
 # ### Create the environment ###
 
-# # # Build basic Docker image on host
-# bash scripts/environment/host_build_docker_image_base.sh
+# # Build basic Docker image on host
+bash scripts/environment/host_build_docker_image_base.sh
 
-# # # Export basic Docker image on host
-# bash scripts/environment/host_export_docker_image_base.sh
+# # Export basic Docker image on host
+bash scripts/environment/host_export_docker_image_base.sh
 
-# # Copy basic Docker image to servers
-# bash scripts/environment/host_push_docker_image_base.sh
+# Copy basic Docker image to servers
+bash scripts/environment/host_push_docker_image_base.sh
 
-# # Load basic Docker image on servers
-# bash scripts/environment/host_build_docker_image_ready_model.sh
-# bash scripts/environment/host_build_docker_image_pipeswitch.sh
-# bash scripts/environment/host_build_docker_image_mps.sh
+# Load basic Docker image on servers
+bash scripts/environment/host_build_docker_image_ready_model.sh
+bash scripts/environment/host_build_docker_image_pipeswitch.sh
+bash scripts/environment/host_build_docker_image_mps.sh
 
 
 # ### Create the environment END ###
