23471de8604ac1114076ee1b0fc8b1ef7d589a9c
eamonmahon <s2602050@ed.ac.uk>
2024-07-01 15:23:21 +0000

    removed verbosity

--- scripts/server_test_script.py
+++ scripts/server_test_script.py
@@ -6,7 +6,7 @@ from scripts.common.util import RunDocker
 def main():
     with RunDocker('pipeswitch:ready_model', 'dev') as rd:
         # Use shell to set the environment variable and run the script
-        rd.run('bash -c "export CUDA_LAUNCH_BLOCKING=1 && python PipeSwitch/scripts/test_script.py"')
+        rd.run('bash python PipeSwitch/scripts/test_script.py')
 
 if __name__ == '__main__':
     main()
