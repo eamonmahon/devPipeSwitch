70c975214f6054f1affdb0652315f7a0a7adf8d6
eamonmahon <s2602050@ed.ac.uk>
2024-07-01 14:41:37 +0000

    fix file path in test scripts

--- scripts/server_test_script.sh
+++ scripts/server_test_script.sh
@@ -1,4 +1,4 @@
 WORK_DIR=$(pwd)/PipeSwitch
 source $WORK_DIR/scripts/config/env.sh
 
-PYTHONPATH=$PYTHONPATH:$WORK_DIR python $WORK_DIR/scripts/environment/server_test_script.py
+PYTHONPATH=$PYTHONPATH:$WORK_DIR python $WORK_DIR/scripts/server_test_script.py
