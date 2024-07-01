72c3e4382636b28c1b09e7aa55eb3dd621c5d9ce
eamonmahon <s2602050@ed.ac.uk>
2024-07-01 14:58:35 +0000

    verbose output for testing

--- scripts/server_test_script.py
+++ scripts/server_test_script.py
@@ -5,7 +5,8 @@ from scripts.common.util import RunDocker
 
 def main():
     with RunDocker('pipeswitch:ready_model', 'dev') as rd:
-        rd.run('python PipeSwitch/scripts/test_script.py')
+        # Set the environment variable and run the script in the same command
+        rd.run('CUDA_LAUNCH_BLOCKING=1 python PipeSwitch/scripts/test_script.py')
 
 if __name__ == '__main__':
     main()
