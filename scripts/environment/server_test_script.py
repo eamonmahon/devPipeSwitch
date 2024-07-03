63fbfcf018920322d7efeafe60898c0dc9df7473
eamonmahon <s2602050@ed.ac.uk>
2024-07-03 10:47:09 +0000

    run simple test with new file layout

--- scripts/environment/server_test_script.py
+++ scripts/environment/server_test_script.py
@@ -5,7 +5,7 @@ from scripts.common.util import RunDocker
 
 def main():
     with RunDocker('pipeswitch:ready_model', 'dev') as rd:
-        rd.run('python PipeSwitch/scripts/test_script.py')
+        rd.run('python PipeSwitch/scripts/environment/test_script.py')
 
 if __name__ == '__main__':
     main()
