2cb12597e2ebacec664b2df003a9c283e6b86ecb
eamonmahon <s2602050@ed.ac.uk>
2024-07-10 11:12:44 +0000

    Revert "changes to test newer versions for environment, model and pytorch - just testing resnet model load"
    This reverts commit 71a2d8a916d27ddb12585367d1d88a83485f8165.

--- scripts/environment/container_download_models.py
+++ scripts/environment/container_download_models.py
@@ -1,8 +1,7 @@
 from task.helper import get_data, get_model
 
 def main():
-    # for model_name in ['resnet152', 'inception_v3', 'bert_base']:
-    for model_name in ['resnet152']:
+    for model_name in ['resnet152', 'inception_v3', 'bert_base']:
         get_model(model_name)
 
 if __name__ == '__main__':
